6fe7161fc015d19e92d571ecc88840aa6fa564f5
Drakontis Panagiotis <drakontis.panagiotis@gmail.com>
2017-10-08 15:13:03 +0300

    Add route to bid_request#create

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  post '/bid', to: 'bid_requests#create', as: :create_bid_request
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
